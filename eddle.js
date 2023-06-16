bfa9d1ef2a96fd0d9de4253c01073e2ce1f5bd83
Adnan Alouane <alouane@usc.edu>
2023-06-15 22:05:34 -0400

    names in boxes

--- eddle.js
+++ eddle.js
@@ -2,14 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
     let csv = document.getElementById("unparsed").innerHTML;
     let playerArr = $.csv.toArrays(csv);
     console.log(playerArr);
+    console.log(playerArr[3]);
+    console.log(playerArr[100]);
+
   
     let options = document.querySelector('.options');
-  
-    for (let i = 2; i < playerArr.length; i++) {
-      let li = document.createElement("li");
-      li.textContent = playerArr[i][0]; // Assuming the name is in the first column
-      options.appendChild(li);
-    }
+
+for (let i = 2; i < playerArr.length; i++) {
+  let li = document.createElement("li");
+  let firstName = playerArr[i][0]; // Get the first name from the first column
+  let lastName = playerArr[i][1]; // Get the last name from the second column
+  let fullName = firstName + " " + lastName; // Concatenate the first and last name
+  li.textContent = fullName;
+  options.appendChild(li);
+}
   
     const selectBox = document.querySelector('.select-box');
     const selectOption = document.querySelector('.select-option');
@@ -22,12 +28,20 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   
     optionList.forEach(function(optionListSingle) {
-      optionListSingle.addEventListener('click', function() {
-        text = this.textContent;
-        soValue.value = text;
-        selectBox.classList.remove('active');
+        optionListSingle.addEventListener('click', function() {
+          var text = this.textContent;
+          soValue.value = text;
+          var boxElements = document.querySelectorAll('[id$="1"], [id$="6"]');
+          for (var i = 0; i < boxElements.length; i++) {
+            if (boxElements[i].textContent.trim() === '') {
+              boxElements[i].textContent = text;
+              break; // Exit the loop after updating the first available box
+            }
+          }
+          selectBox.classList.remove('active');
+        });
       });
-    });
+      
   
     optionSearch.addEventListener('keyup', function() {
       var filter, li, i, textValue;
